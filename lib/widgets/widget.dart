0fc81c541b6eb39e1a3a3559c212db5f7e12a012
Nuel Young <nuelmega@gmail.com>
2022-12-24 06:09:12 -0800

    route

---
+++ lib/widgets/widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+goToPush(BuildContext context, page) {
+  return Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => page,
+    ),
+  );
+}
+
+goToReplace(BuildContext context, page) {
+  return Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(
+      builder: (context) => page,
+    ),
+  );
+}
+
+goToPop(BuildContext context, page) {
+  return Navigator.pop(
+    context,
+    MaterialPageRoute(
+      builder: (context) => page,
+    ),
+  );
+}
