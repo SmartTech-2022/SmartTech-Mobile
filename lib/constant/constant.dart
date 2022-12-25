c506a8c3a66284ea8248d0ad7cfe2f5d4a8335f5
Nuel Young <nuelmega@gmail.com>
2022-12-24 23:42:13 -0800

    .

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-//class Mycolor {
  var kPrimarycolor = const Color(0xff027314);
  var kPrimarycolorlight = const Color(0xffC9D7CB);
  var kSecondarycolor = const Color(0xffffffff);
 var kBlackcolor = const Color(0xff000000);
 var bold = FontWeight.bold;
-//}
+
