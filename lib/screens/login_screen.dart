da7ca4bb74ad76fe59052e3623cf9880ad8d7b09
Nuel Young <nuelmega@gmail.com>
2022-12-28 03:15:20 -0800

    .

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:onevote/constant/constant.dart';
+import 'package:onevote/widgets/my_text_button.dart';
+import 'package:onevote/widgets/my_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,8 +12,101 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final vin = TextEditingController();
+  final password = TextEditingController();
+  bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        color: kSecondarycolor,
+        padding: EdgeInsets.only(
+            left: 20, right: 20, top: screenHeight(context) * 0.25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Welcome Back",
+              style:
+                  TextStyle(color: kBlackcolor, fontSize: 30, fontWeight: bold),
+            ),
+            Text(
+              "Please enter your details to continue",
+              style: TextStyle(
+                color: kPrimarycolor,
+                fontSize: 18,
+              ),
+            ),
+            const Gap(50),
+            Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    MyTextField(
+                      controller: vin,
+                      hintText: "VIN",
+                    ),
+                    const Gap(30),
+                    MyTextField(
+                      controller: password,
+                      hintText: "Password",
+                      isObscure: _obscureText,
+                      suffixIcon: IconButton(
+                        icon: Icon(_obscureText
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          togglestate();
+                        },
+                      ),
+                    ),
+                    const Gap(30),
+                    MyTextButton(
+                      onTap: () {},
+                      text: "Log in",
+                    ),
+                    const Gap(30),
+                    GestureDetector(
+                      child: Text(
+                        "Forgot Password?",
+                        style: TextStyle(color: kPrimarycolor, fontSize: 20),
+                      ),
+                    ),
+                    Gap(screenHeight(context) - 780),
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don't have an account?",
+                            style:
+                                TextStyle(fontSize: 20, color: kPrimarycolor),
+                          ),
+                          const Gap(5),
+                          Text(
+                            "Create Account",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: kPrimarycolor,
+                                fontWeight: bold),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void togglestate() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
   }
 }
---
+++ lib/widgets/my_text_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:onevote/constant/constant.dart';
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String? hintText;
+  final bool? isObscure;
+  final IconData? icon;
+  final Widget? suffixIcon;
+
+  const MyTextField(
+      {this.controller,
+      this.hintText,
+      this.isObscure = false,
+      this.icon,
+      this.suffixIcon,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: isObscure ?? false,
+      decoration: InputDecoration(
+          suffixIcon: suffixIcon,
+          prefixIconColor: kPrimarycolor,
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 40.0, 15.0),
+          iconColor: kPrimarycolor,
+          hintText: hintText,
+          hintStyle: TextStyle(color: kPrimarycolor),
+          fillColor: const Color.fromARGB(255, 254, 251, 251),
+          filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(color: kPrimarycolor),
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(color: kPrimarycolor))),
+    );
+  }
+}
