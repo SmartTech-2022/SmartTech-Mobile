32df9ae4ebe72559fa55aa66b50a2950e9993d44
Gebor95 <josephokosisi@gmail.com>
2023-01-13 15:03:22 +0100

    Tkay

--- lib/utils/validator.dart
+++ lib/utils/validator.dart
@@ -3,26 +3,27 @@ abstract class Validator {
     return em.length > 4 ? true : false;
   }
 
-      String? validateVin(String? fieldContent) {
+  String? validateVin(String? fieldContent) {
     String value = fieldContent ?? '';
     if (value.isNotEmpty && value.length > 4) {
-          fieldContent = value;
-        } else if (value.isEmpty) {
-          return "This field can't be empty";
-        } else {
-          return "VIN is invalid";
-        }
-        return null;
+      fieldContent = value;
+    } else if (value.isEmpty) {
+      return "This field can't be empty";
+    } else {
+      return "VIN is invalid";
+    }
+    return null;
   }
+
   String? validatePassword(String? fieldContent) {
     String value = fieldContent ?? '';
     if (value.isNotEmpty && isPassword(value)) {
-          fieldContent = value;
-        } else if (value.isEmpty) {
-          return "This field can't be empty";
-        } else {
-          return "VIN is invalid";
-        }
-        return null;
+      fieldContent = value;
+    } else if (value.isEmpty) {
+      return "This field can't be empty";
+    } else {
+      return "Password is invalid";
+    }
+    return null;
   }
 }
